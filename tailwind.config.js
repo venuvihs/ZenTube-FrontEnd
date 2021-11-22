493cdeef35e8703fc31729a86085f4a170d0db43
greendog21 <69486663+greendog21@users.noreply.github.com>
2021-11-22 07:21:54 -0600

    tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+// tailwind.config.js
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+
+        'custom-blue': "#007f97",
+        "custom-light-blue": "#a4bcbc",
+        "custom-pink": "#7c587f",
+        "custom-purple": "#4c3f77",
+        "custom-black": "#051126",
+        "dark-bg": "#02023B",
+        "bg-result": "#07032B",
+
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
